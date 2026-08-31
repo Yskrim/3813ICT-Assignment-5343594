195ff0ef5654195c4eb578fa1685a46bb17a2b7e
Yskrim <141317956+Yskrim@users.noreply.github.com>
2026-09-01 00:04:01 +1000

    fix login and nav

--- project/src/app/app.ts
+++ project/src/app/app.ts
@@ -1,14 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { User, AdminRequest, Group, Channel } from './models'
+import { AuthService } from './services/auth.service';
 
 
 @Component({
-  imports: [ RouterOutlet, RouterLink ],
+  imports: [ RouterLink, RouterOutlet],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('project');
+
+  user: User | null = null;
+
+  constructor(
+    protected auth: AuthService,
+    private router: Router,
+
+  ) {}
+
+  ngOnInit(): void {
+    const currentUser = this.auth.currentUser();
+    if (!currentUser) {
+      this.router.navigate(['/login']);
+      return;
+    }
+  }
 }
